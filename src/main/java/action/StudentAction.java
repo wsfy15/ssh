7aa65903663f46c7ac8d15f47aa662bdabd8c71f
wsfy15 <2682484253@qq.com>
2018-11-14 10:38:53 +0800

    fix vulnerabilities

--- src/main/java/action/StudentAction.java
+++ src/main/java/action/StudentAction.java
@@ -1,10 +1,12 @@
 package action;
 
+import com.opensymphony.xwork2.ModelDriven;
 import entity.Student;
 import service.StudentService;
 
-public class StudentAction {
+public class StudentAction implements ModelDriven<Student> {
     private StudentService studentService;
+    private Student student;
 
     public void setStudentService(StudentService studentService) {
         this.studentService = studentService;
@@ -12,10 +14,15 @@ public class StudentAction {
 
     public String add(){
         Student s = new Student();
-        s.setId(100);
+        s.setId(200);
         s.setName("小明");
         s.setPasswd("123");
         this.studentService.add(s);
         return "success";
+    }
+
+    @Override
+    public Student getModel() {
+        return student;
     }
 }
